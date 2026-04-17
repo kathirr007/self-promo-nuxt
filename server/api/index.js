f84749fb3a827b5936fc9b838ea1f4c741fe1ad4
Kathirr007 <kathirr007@gmail.com>
2026-04-17 21:00:11 +0530

    updated vercel.json

---
+++ server/api/index.js
@@ -0,0 +1,8 @@
+// api/index.js
+const { createServer } = require('http')
+const app = require('../routes/index') // adjust path if needed
+
+module.exports = (req, res) => {
+  // If your server/routes/index exports an express/nuxt handler:
+  return app(req, res)
+}
