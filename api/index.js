368ceb2ce6e9535856440471de49556616af77dc
Kathirr007 <kathirr007@gmail.com>
2026-04-17 21:37:45 +0530

    updated api/index

--- api/index.js
+++ api/index.js
@@ -1,8 +1,4 @@
 // api/index.js
-const { createServer } = require('http')
-const app = require('../server/index') // adjust path if needed
 
-module.exports = (req, res) => {
-  // If your server/routes/index exports an express/nuxt handler:
-  return app(req, res)
-}
+
+module.exports = require('../server/index')
